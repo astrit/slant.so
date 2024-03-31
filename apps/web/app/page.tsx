156fcb31fd64d01c8a6f88907e3d668b525373be
astrit <am@astrit.co>
2024-03-31 04:13:54 +0200

    Msg → Sun Mar 31 04:13:54 CEST 2024

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,14 +9,14 @@ export default function HomePage() {
       <h1>{`SLΛ\\T`}</h1>
       <footer>
         <span>
-          <Link href="https://lona.is">L O \ A</Link>
+          <Link href="https://lona.is">{`L O \\ A`}</Link>
         </span>
-        <div>×</div>
+        <div>{`×`}</div>
         <span>
-          <Link href="https://astrit.co">IIIIII</Link>
+          <Link href="https://astrit.co">{`IIIIII`}</Link>
         </span>
-        <div>~</div>
-        <time>2024</time>
+        <div>{`~`}</div>
+        <time>{`2024`}</time>
       </footer>
     </Home>
   )
