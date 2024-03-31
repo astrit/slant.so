3d030a218313c2461e23543762ea454c1a4a53c6
astrit <am@astrit.co>
2024-03-31 01:50:22 +0100

    add flex props

--- packages/core/src/flex.tsx
+++ packages/core/src/flex.tsx
@@ -6,6 +6,15 @@ interface FlexProps {
   children: ReactNode;
   className?: string;
   appName: string;
+  span: string;
+  rows: string;
+  flex: string;
+  order: string;
+  align: string;
+  justify: string;
+  ratio: string;
+  padding: string;
+  direction: string;
 }
 
 //
