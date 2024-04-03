d0771046971401e40bf65f66994876154718c0ef
astrit <am@astrit.co>
2024-04-03 16:20:04 +0200

    Update home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,16 @@
-import Home from "&/home/home";
-import Link from "@/link/link";
+import Link from "@/link/link"
+import Home from "&/home/home"
 
 export default function Page(): JSX.Element {
   return (
     <Home>
-      <h1>{`SLΛ\\T`}</h1>
+      <h1>
+        <span>{`S`}</span>
+        <span>{`L`}</span>
+        <span>{`Λ`}</span>
+        <span>{`\\`}</span>
+        <span>{`T`}</span>
+      </h1>
       <footer>
         <span>
           <Link href="https://lona.is">{`L O \\ Λ`}</Link>
@@ -15,5 +21,5 @@ export default function Page(): JSX.Element {
         </span>
       </footer>
     </Home>
-  );
+  )
 }
