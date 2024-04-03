dc21275c5c2b238b39b998fe92e449b8220c3df0
astrit <am@astrit.co>
2024-04-03 12:36:49 +0200

    Slant 0.1.0

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,21 @@
+import Home from "&/home/home";
+import Link from "@/link/link";
+
 export default function Page(): JSX.Element {
-  return <code>{`S L Λ﹨T`}</code>;
+  return (
+    <Home>
+      <h1>{`SLΛ\\T`}</h1>
+      <footer>
+        <span>
+          <Link href="https://lona.is">{`L O \\ A`}</Link>
+        </span>
+        <div>{`×`}</div>
+        <span>
+          <Link href="https://astrit.co">{`IIIIII`}</Link>
+        </span>
+        {/* <div>{`~`}</div>
+        <time>{`2024`}</time> */}
+      </footer>
+    </Home>
+  );
 }
---
+++ apps/web/design/block/header/header.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import Nav from "@/nav/nav";
+import Link from "@/link/link";
+
+import "@/header/header.css";
+
+function Left({ children }: { children: ReactNode }) {
+  return <div className="sides left">{children}</div>;
+}
+
+function Right({ children }: { children: ReactNode }) {
+  return <div className="sides right">{children}</div>;
+}
+
+export default function Header() {
+  const path = usePathname();
+
+  return (
+    <header className="main-header">
+      <Left>
+        <Nav />
+      </Left>
+      <small></small>
+      <Right>{path !== "/about" && <Link href="/about">~ 2024</Link>}</Right>
+    </header>
+  );
+}
