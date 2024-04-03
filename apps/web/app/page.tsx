75c29a49dd9b11b87c5070f873e05deea7be7079
astrit <am@astrit.co>
2024-04-03 12:38:20 +0200

    Msg → Wed Apr  3 12:38:20 CEST 2024

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,14 +7,12 @@ export default function Page(): JSX.Element {
       <h1>{`SLΛ\\T`}</h1>
       <footer>
         <span>
-          <Link href="https://lona.is">{`L O \\ A`}</Link>
+          <Link href="https://lona.is">{`L O \\ Λ`}</Link>
         </span>
         <div>{`×`}</div>
         <span>
           <Link href="https://astrit.co">{`IIIIII`}</Link>
         </span>
-        {/* <div>{`~`}</div>
-        <time>{`2024`}</time> */}
       </footer>
     </Home>
   );
